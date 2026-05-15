689ccdcd429d547a67f8914a4f3661b280a54562
The Fahim Ahamed <fahimahamed1822@gmail.com>
2026-05-15 09:41:45 +0600

    Array Easy .cpp 05-15-2026

---
+++ 0704-binary-search/0704-binary-search-05-15-2026-09-41-44.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it = lower_bound(nums.begin(),nums.end(),target);
+        if(it == nums.end())
+        return -1;
+        else if(*it != target)
+        return -1;
+        return it-nums.begin();
+    }
+};
